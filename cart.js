9fe3b7497441a207ad339174eb5f95b798284602
JL Shop 06 <greentherapy06@gmail.com>
2026-05-03 13:24:33 +0100

    Implement add-to-cart functionality in cart.js

---
+++ cart.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".add-to-cart");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const product = {
+        name: button.dataset.name,
+        price: button.dataset.price
+      };
+
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      cart.push(product);
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      alert(product.name + " ajouté au panier");
+    });
+  });
+});
